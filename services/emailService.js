8f511c087f4fccd0465e1cb86591344f83274869
Adam Kwiatek <a.kwiatek@kainos.com>
2017-03-31 09:47:55 +0200

    IDP email subjects
    * Added missing email subjects for emails which are sent from IDP
    * Separate constants for checked email subjects were replaced with 2
      arrays of email subjects: 1 for IDAM + 1 for IDP.
    * Added missing semicolons.
    * Removed unusued functions.

--- services/emailService.js
+++ services/emailService.js
@@ -6,31 +6,41 @@ var Q = require('q');
 
 module.exports = function () {
 
-    var idam_orgadmin_created='New user added on Common Platform';
-    var idam_orgadmin_rereg='User re-registered on Common Platform';
-    var idam_org_dereg='Organisation deregistered on Common Platform';
-    var idam_org_updated='Organisation details have changed on Common Platform';
-    var idam_org_accountableperson_updated='Accountable person details have changed on Common Platform';
-    var idam_org_informlist_updated='Email notification list updated on Common Platform';
-    var idam_org_rereg='Organisation re-registered on Common Platform';
-    var idam_cppadmin_created='New user added on Common Platform';
-    var idam_cppadmin_rereg='User re-registered on Common Platform';
-    var idam_user_created='New user added on Common Platform';
-    var idam_user_dereg='User deregistered on Common Platform';
-    var idam_user_dereg_for_user='Account deregistered on Common Platform';
-    var idam_user_name_updated='Change of name on your Common Platform account';
-    var idam_user_rereg='User re-registered on Common Platform';
-    var idam_user_role_changed='User changed to an Administrator on Common Platform';
+    var IDAM_SUBJECTS = [
+        'Account deregistered on Common Platform',
+        'Accountable person details have changed on Common Platform',
+        'Change of name on your Common Platform account',
+        'Email notification list updated on Common Platform',
+        'New user added on Common Platform',
+        'Organisation deregistered on Common Platform',
+        'Organisation details have changed on Common Platform',
+        'Organisation re-registered on Common Platform',
+        'User changed to an Administrator on Common Platform',
+        'User deregistered on Common Platform',
+        'User re-registered on Common Platform',
+    ];
+
+    var IDP_SUBJECTS = [
+        'Account locked on Common Platform',
+        'Authentication device added to your Common Platform account',
+        'Authentication device removed from your Common Platform account',
+        'Change of email address and username on your Common Platform account',
+        'Change of password on your Common Platform account',
+        'Mobile phone number changed on Common Platform',
+        'Re-registration of your Common Platform Account',
+        'Register Your Common Platform Account',
+        'Reset Your Common Platform Account',
+    ];
 
     var getIdamLatestEmail = function (userEmail, subject) {
         return getLatestEmail(core.getConfig().emailMockService.url, userEmail, subject).then(function (response) {
             var newlinesRegex = /\n/g;
-            var doubleDots = /\.\./g;
+            var doubleDotsRegex = /\.\./g;
 
             // Workaround for newline not properly escaped by IDP Email Stub (replacing them with <br /> as will be html)
             // Workaround for issue in ISG Email Stub where dots are sometimes duplicated (due to that registration link gets broken)
             return JSON.parse(
-                response.body.replace(doubleDots, '.').replace(newlinesRegex, '<br />')
+                response.body.replace(doubleDotsRegex, '.').replace(newlinesRegex, '<br />')
             )
         })
     };
@@ -80,64 +90,22 @@ module.exports = function () {
         if (tokenMatches) {
             return tokenMatches[1];
         } else {
-            throw new Error("Could not retrieve registration token from email" + emailBody);
+            throw new Error("Could not retrieve registration token from email:" + emailBody);
         }
     };
 
-    this.getResetPasswordMail = function (userEmail) {
-        return getIdamLatestEmail(userEmail, 'Reset Your Common Platform Account');
-    };
-
-    this.getUserRegistrationMail = function (userEmail) {
-        return getIdamLatestEmail(userEmail, 'Register Your Common Platform Account');
-    };
-
-    this.getReregisterMail = function (userEmail) {
-        return getIdamLatestEmail(userEmail, 'Re-registration of your Common Platform Account');
-    };
-
-    this.getAccountablePersonUpdatedMail = function (userEmail) {
-        return getIdamLatestEmail(userEmail, idam_org_accountableperson_updated);
-    };
-
-    this.getInformListUpdatedMail = function (userEmail) {
-        return getIdamLatestEmail(userEmail, idam_org_informlist_updated);
-    };
-
-    this.getNewCommonPlatformAdministratorCreatedMail = function (userEmail) {
-        return getIdamLatestEmail(userEmail, idam_cppadmin_created);
-    };
-
-    this.getNewCommonPlatformSupportAdministratorCreatedMail = function (userEmail) {
-        return getIdamLatestEmail(userEmail, idam_orgadmin_created);
-    };
-
-    console.log('Following email subjects  do not differ any more: ' +
-                 idam_orgadmin_created + idam_cppadmin_created +
-                 idam_orgadmin_rereg   + idam_cppadmin_rereg);
-
     this.getAllEmails = function (userEmail) {
-        return Q.all([
-            this.getResetPasswordMail(userEmail),
-            this.getReregisterMail(userEmail),
-            this.getUserRegistrationMail(userEmail),
-            this.getAccountablePersonUpdatedMail(userEmail),
-            this.getInformListUpdatedMail(userEmail),
-            getIdamLatestEmail(userEmail, idam_org_dereg),
-            getIdamLatestEmail(userEmail, idam_org_updated),
-            getIdamLatestEmail(userEmail, idam_org_rereg),
-            getIdamLatestEmail(userEmail, idam_user_created),
-            getIdamLatestEmail(userEmail, idam_user_dereg),
-            getIdamLatestEmail(userEmail, idam_user_dereg_for_user),
-            getIdamLatestEmail(userEmail, idam_user_name_updated),
-            getIdamLatestEmail(userEmail, idam_user_rereg),
-            getIdamLatestEmail(userEmail, idam_user_role_changed)
-        ]).then(function (emails) {
-            console.log(emails)
-            return emails.filter(function (email) {
-                return email.Body !== undefined;
-            })
-        })
+        var requests = IDP_SUBJECTS.concat(IDAM_SUBJECTS)
+            .map(function (subject) {
+                return getIdamLatestEmail(userEmail, subject);
+            });
+        return Q.all(requests)
+            .then(function (emails) {
+                console.log(emails);
+                return emails.filter(function (email) {
+                    return email.Body !== undefined;
+                });
+            });
     };
 
 };
